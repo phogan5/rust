99542ee913ce80884f26dd171a7f6164bc3bdcbb
Phil <philiphogan10@gmail.com>
2023-07-15 19:11:06 -0400

    initial commit

---
+++ functions/target/debug/deps/functions-17ac82308895f22b.d
@@ -0,0 +1,5 @@
+/home/phogan/code/github/rust/functions/target/debug/deps/functions-17ac82308895f22b: src/main.rs
+
+/home/phogan/code/github/rust/functions/target/debug/deps/functions-17ac82308895f22b.d: src/main.rs
+
+src/main.rs:
