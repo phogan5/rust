d695d68fe1e1a3951cd2cacd6daa6bbd491c2e55
Phil <philiphogan10@gmail.com>
2023-07-15 22:47:50 -0400

    different types of loops

---
+++ control_flow/src/main.rs
@@ -0,0 +1,87 @@
+fn main() {
+    let number =3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number = 6;
+
+    if number % 4 == 0{
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 2, 3, or 4");
+    }
+    
+
+    let condition = true;
+    let number = if condition {5} else {6};
+
+    println!("The value of number is {number}");
+    //Because if is an expression, it can be used on the right side of a let statement to assign a
+    //variable
+
+
+    //loop 
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    //multi-layered, labelled loops
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+    
+    //while
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    //for
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+    //iterating through an array
+    
+    for number in (1..4).rev() {
+        println!("{number}...");
+    }
+    println!("LIFTOFF!!!");
+    //Executing code a certain number of times
+
+
+
+}
---
+++ control_flow/target/debug/control_flow.d
@@ -0,0 +1,1 @@
+/home/phogan/code/github/rust/control_flow/target/debug/control_flow: /home/phogan/code/github/rust/control_flow/src/main.rs
