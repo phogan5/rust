dd4c0b51c00ddde3ea84f13e23dace71398cb853
Phil <philiphogan10@gmail.com>
2023-08-01 22:04:43 -0400

    modules and submodules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,25 +1,15 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
+pub use crate::front_of_house::hosting;
 pub fn eat_something_at_restaurant() {
     //Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     //Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    //use path
+    hosting::add_to_waitlist();
 }
 
 mod back_of_house {
