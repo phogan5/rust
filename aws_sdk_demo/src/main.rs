122e287fffa86f9a6df0416a2407a2d566381bc5
phogan5 <philiphogan10@gmail.com>
2024-01-09 21:36:51 -0500

    random rust stuff

---
+++ aws_sdk_demo/src/main.rs
@@ -0,0 +1,29 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_config::BehaviorVersion;
+use aws_sdk_dynamodb::{Client, Error};
+
+//Lists ddb tables
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::defaults(BehaviorVersion::latest())
+    .region(region_provider)
+    .load()
+    .await;
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await?;
+
+    println!("Tables:");
+
+    let names = resp.table_names();
+
+    for name in names {
+        println!(" {}", name)
+    }
+
+    println!();
+    println!("Found {} tables", names.len());
+
+    Ok(())
+}
